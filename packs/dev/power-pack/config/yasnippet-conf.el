fae2bd15391f2d4edd9e899195c9103747e36894
anks <michal.anks@gmail.com>
2015-01-05 03:13:28 +0100

    yas conf update ( now includes basic snippets)

--- packs/dev/power-pack/config/yasnippet-conf.el
+++ packs/dev/power-pack/config/yasnippet-conf.el
@@ -1,5 +1,7 @@
 (live-add-pack-lib "yasnippet")
 (require 'yasnippet)
 (setq live-yasnippet-dir (concat live-etc-dir "snippets"))
-(setq yas-snippet-dirs `(,live-yasnippet-dir))
+(setq basic-yasnippet-dir (concat live-etc-dir "basic-snippets"))
+(setq yas-snippet-dirs `(,live-yasnippet-dir
+                         ,basic-yasnippet-dir))
 (yas-global-mode 1)
