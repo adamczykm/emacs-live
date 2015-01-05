96c5a8109e1c0ef4dccba1fd1aded6a1c09e19b1
anks <michal.anks@gmail.com>
2015-01-05 02:56:49 +0100

    python-pack added.

---
+++ packs/stable/python-pack/config/python-mode-conf.el
@@ -0,0 +1,9 @@
+(live-add-pack-lib "python-mode")
+(require 'python-mode)
+(when (featurep 'python) (unload-feature 'python t))
+
+
+(define-key python-mode-map (kbd "C-c C-b") nil)
+(setq py-shell-name "ipython")
+
+(py-split-windows-on-execute-off)
