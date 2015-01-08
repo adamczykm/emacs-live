c2b556673e2b5ac2e8b37af342ec8c3b81bcc527
anks <michal.anks@gmail.com>
2015-01-08 13:29:38 +0100

    last commit doesnt "build". fix

--- packs/stable/bindings-pack/config/new-bindings.el
+++ packs/stable/bindings-pack/config/new-bindings.el
@@ -124,17 +124,20 @@
 
 (define-key haskell-mode-map (kbd "C-c b") nil)
 
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-(define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-(define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
-(define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
-(define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
 ;;** Structured Haskell Mode
 (define-key shm-map (kbd "M-j") 'shm/newline-indent)
