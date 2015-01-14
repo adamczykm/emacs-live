05a8069932919641e942594f66aafa743f75c720
anks <michal.anks@gmail.com>
2015-01-15 00:23:27 +0100

    haskell indentation 2

---
+++ packs/dev/haskell-pack/config/hi2-conf.el
@@ -0,0 +1,3 @@
+(live-add-pack-lib "hi2")
+(require 'hi2)
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
--- packs/dev/haskell-pack/init.el
+++ packs/dev/haskell-pack/init.el
@@ -4,5 +4,10 @@
 ;; See README for more information.
 
 (live-load-config-file "haskell-mode-conf.el")
-(live-load-config-file "shm-conf.el")
+
 (live-load-config-file "ghc-mod-conf.el")
+
+
+;;* indentations
+;; (live-load-config-file "shm-conf.el") ; structured haskell mode
+(live-load-config-file "hi2-conf.el") ; haskell indentation 2
